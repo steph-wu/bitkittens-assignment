96100f746eb9e30469c5236bd2d6f2c2397067c8
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-02-27 14:10:49 -0500

    Insert image of cat to cat-box divs each time summon cats is clicked

--- cats.js
+++ cats.js
@@ -1,8 +1,21 @@
-$(document).ready(function() {
+$(document).ready(function(){
 
-$('.summon-cats').on('click', function() {
-
-  console.log('Summon cats clicked!');
-
+  $('.summon-cats').on('click', function() {
+    $.ajax({
+      url: 'http://bitkittens.herokuapp.com/cats.json',
+      method: 'GET',
+      dataType: 'json'
+    }).done(function(responseData) {
+      var cats = responseData.cats;
+      $('#cat1').html(
+        '<img src="' + cats[0].photo + '" alt="Photo of ' + cats[0].name + '">');
+      $('#cat2').html(
+        '<img src="' + cats[1].photo + '" alt="Photo of ' + cats[1].name + '">');
+      $('#cat3').html(
+        '<img src="' + cats[2].photo + '" alt="Photo of ' + cats[2].name + '">');
+    }).always(function() {
+      console.log('Ajax request sent!');
+    });
   });
+
 });
